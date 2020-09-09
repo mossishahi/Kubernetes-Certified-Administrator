8b23765ce7da43f2514d013e82ed9d81aa8e5d20
Behrad Eslamifar <b.eslamifar@gmail.com>
2020-09-09 20:26:38 +0430

    kube state metrics

--- labs/build_image/ubuntu-1804/setup/install-minimum-package.sh
+++ labs/build_image/ubuntu-1804/setup/install-minimum-package.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+# disable translate download and add proxy
+cat <<EOF | sudo tee /etc/apt/apt.conf.d/20translate
+Acquire::Languages "none";
+EOF
+
 sudo apt-get update 
 sudo apt-get install -y apt-transport-https curl ca-certificates software-properties-common gnupg2 vim traceroute bash-completion
 sudo apt-get upgrade -y
---
+++ labs/monitoring/README.md
@@ -0,0 +1,7 @@
+# kube-state-metrics 
+Documentation can be found in Github
+```
+https://github.com/kubernetes/kube-state-metrics
+```
+
+
