cd1c283f849dc2217a5e29f1baa9152a903d18c1
Behrad Eslamifar <b.eslamifar@gmail.com>
2020-11-28 17:37:19 +0330

    calico version

--- labs/vagrant/multi-master/init-k8s.sh
+++ labs/vagrant/multi-master/init-k8s.sh
@@ -7,7 +7,7 @@ if [ "$NODE" == "1" ] then
 	kubeadm init --kubernetes-version 1.18.8 --apiserver-advertise-address $IP
 	
 	wget https://docs.projectcalico.org/v3.14/manifests/calico.yaml
-	sed -i 's/3.14.[0-9]\+/3.14.1/g' calico.yaml
+	sed -i 's/3.14.[0-9]\+/3.14.2/g' calico.yaml
 	kubectl --kubeconfig /etc/kubernetes/admin.conf create -f calico.yaml
 fi
 
